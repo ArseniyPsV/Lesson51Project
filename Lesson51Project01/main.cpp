8e4f9f54294cc10de39c0cfb9cf266549e5a9cee
Arseniy PSV <arsypsv77@gmail.com>
2024-03-25 18:40:32 +0300

    Add project files.

---
+++ Lesson51Project01/main.cpp
@@ -0,0 +1,15 @@
+#include "logic.h"
+#include "students.h"
+
+int main() {
+	Students st1;
+	Students st2;
+
+	int x = 1;
+	int y = 21;
+	int z = 10;
+
+	cout << sum(x, y) << endl;
+
+	return 0;
+}
---
+++ Lesson51Project01/students.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Students {
+	string name;
+	int age;
+	double mark;
+};
